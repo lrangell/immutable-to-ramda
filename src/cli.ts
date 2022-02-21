b0cc71c8c699faa56852b3a59e5bb7d487bfd3e7
Lucas Rangel <lucas@rangel.dev>
2022-02-21 18:51:28 -0300

    Add dry run support

--- src/cli.ts
+++ src/cli.ts
@@ -1,19 +1,39 @@
 import {program} from 'commander'
-import {parse, print, visit} from 'recast'
+import {parse, print, visit, prettyPrint} from 'recast'
 import transformers from './transformers'
 import {promises} from 'fs'
 import {tryCatch} from 'ramda'
+import {diffLines} from 'diff'
+const colors = require('colors')
 
-const rewriteSource = async (filepath: string) => {
-  const src = await promises.readFile(filepath, 'utf-8')
+const transformSource = async (path: string) => {
+  const src = await promises.readFile(path, 'utf-8')
   const ast = parse(src, {parser: require('recast/parsers/typescript')})
   visit(ast, transformers)
   const newSrc = print(ast).code
-  if (src === newSrc) return
-  const newFh = await promises.open(filepath, 'w')
-  newFh.writeFile(print(ast).code)
-  console.log(`${filepath} written`)
+  if (src === newSrc) return false
+  return {src, newSrc}
 }
+
+const printDiff = async (path: string) => {
+  const code = await transformSource(path)
+  if (!code) return
+  console.log(code)
+  diffLines(code.src, code.newSrc).forEach((part) => {
+    const color = part.added ? 'green' : part.removed ? 'red' : 'grey'
+    console.log(part.value[color])
+  })
+}
+
+const rewriteSource = async (path: string) => {
+  const code = await transformSource(path)
+  if (!code) return
+  const newFh = await promises.open(path, 'w')
+  newFh.writeFile(code.newSrc)
+  console.log(`${path} written`)
+}
+
+program.option('--dry')
 const args = program.parse(process.argv)
 
-args.args.forEach(tryCatch(rewriteSource, () => ''))
+args.args.forEach(program.opts().dry ? printDiff : rewriteSource)
